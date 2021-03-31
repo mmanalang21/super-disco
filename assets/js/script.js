b1e175274844ef48a7d4acd0999ceaf4c8ee64e8
Matthew <pinoycc05@aol.com>
2021-03-30 17:17:58 -0700

    create for loop to iterate over IDs

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,23 @@ $(".saveBtn").on("click", function () {
   console.log('textData', textData, timeClock);
   localStorage.setItem(timeClock, textData);
 });
+
+
+let currentHour = moment().hours();
+
+// create a for loo to iterate through all HTML IDs
+
+for (let i = 9; i <= 17; i++) {
+  $('#' + i).children('textarea').val(localStorage.getItem(i)) 
+
+  if (i < currentHour) {
+    $("#" + i).children('textarea').addClass("past")
+
+  } else if (currentHour === i) {
+    $("#" + i).children('textarea').addClass("present")
+  }
+
+  else {
+    $("#" + i).children('textarea').addClass("future")
+  }
+}
